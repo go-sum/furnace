11c4d50ba546be0de017788f6dfe8fbd90318fff
Johan Meyer <y-core@outlook.com>
2026-05-04 06:48:23 +0200

    Add version info to binary

--- internal/cli/root.go
+++ internal/cli/root.go
@@ -1,6 +1,13 @@
 package cli
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// Version is set at build time via -ldflags "-X github.com/go-sum/furnace/internal/cli.Version=vX.Y.Z".
+var Version = "dev"
 
 func NewRootCommand() *cobra.Command {
 	var configPath string
@@ -8,6 +15,8 @@ func NewRootCommand() *cobra.Command {
 	root := &cobra.Command{
 		Use:          "furnace",
 		Short:        "Secure VPS deployment agent",
+		Long:         fmt.Sprintf("Secure VPS deployment agent %s", Version),
+		Version:      Version,
 		SilenceUsage: true,
 	}
 
